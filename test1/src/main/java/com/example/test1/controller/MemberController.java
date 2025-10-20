693378392c6e258261c22c1beda104729a14557a
harford <jhsimple4@gmail.com>
2025-10-20 15:03:50 +0900

    2025.10.20

--- test1/src/main/java/com/example/test1/controller/MemberController.java
+++ test1/src/main/java/com/example/test1/controller/MemberController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,8 +26,16 @@ public class MemberController {
 	@Autowired
 	MemberService memberService;
 	
+	@Value("${client_id}")
+	private String client_id;
+
+    @Value("${redirect_uri}")
+    private String redirect_uri;
+	
 	@RequestMapping("/member/login.do") 
     public String login(Model model) throws Exception{ 
+		String location = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id="+client_id+"&redirect_uri="+redirect_uri;
+        model.addAttribute("location", location);
 		
         return "/member/member-login";
     }
